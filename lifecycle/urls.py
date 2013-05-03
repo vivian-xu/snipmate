76169c7706d6a927c858ea336225f3e98a2ba871
Jackon Yang <jiekunyang@gmail.com>
2013-05-03 14:03:56 +0800

    url and views simple

--- lifecycle/urls.py
+++ lifecycle/urls.py
@@ -1,17 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url  # include
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+# url(r'^admin/', include(admin.site.urls)),
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'views.home', name='home'),
-    # url(r'^django_example/', include('django_example.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^$', 'views.home', name='home'),
+                       )
--- lifecycle/views.py
+++ lifecycle/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+
+def home(request):
+    print("method: {}".format(request.method))
+    print('post: {}'.format(request.POST))
+    print("request: {}".format(request.REQUEST))
+    print("get: {}".format(request.GET))
+    paras = dict()
+    paras['author'] = 'JackonYang'
+
+    return render_to_response('homepage.html', paras)
